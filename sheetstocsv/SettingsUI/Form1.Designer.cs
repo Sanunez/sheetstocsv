95b9d3b2054510449b12d33c32b550587d16c214
Sergio Nuñez <nunez.sergio95@gmail.com>
2017-09-27 09:04:57 -0500

    UI Design
    Finished UI Design and need to implement Save Button

--- sheetstocsv/SettingsUI/Form1.Designer.cs
+++ sheetstocsv/SettingsUI/Form1.Designer.cs
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.OutputDirectory_Label = new System.Windows.Forms.TextBox();
+            this.Outputdirectory_Label = new System.Windows.Forms.Label();
+            this.OutputDirectory_TextBox = new System.Windows.Forms.TextBox();
             this.OutputDirectoryBrowse_Button = new System.Windows.Forms.Button();
             this.SpreadsheetID_Label = new System.Windows.Forms.Label();
             this.SpreadsheetID_Textbox = new System.Windows.Forms.TextBox();
@@ -39,6 +38,9 @@
             this.Columns_Button = new System.Windows.Forms.Label();
             this.Columns_NumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.Columns_DataGridView = new System.Windows.Forms.DataGridView();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.IdPaste_Button = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             ((System.ComponentModel.ISupportInitialize)(this.Columns_NumericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Columns_DataGridView)).BeginInit();
             this.SuspendLayout();
@@ -50,38 +52,39 @@
             this.label1.Size = new System.Drawing.Size(100, 23);
             this.label1.TabIndex = 0;
             // 
-            // label2
+            // Outputdirectory_Label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Output Directory";
+            this.Outputdirectory_Label.AutoSize = true;
+            this.Outputdirectory_Label.Location = new System.Drawing.Point(12, 9);
+            this.Outputdirectory_Label.Name = "Outputdirectory_Label";
+            this.Outputdirectory_Label.Size = new System.Drawing.Size(84, 13);
+            this.Outputdirectory_Label.TabIndex = 1;
+            this.Outputdirectory_Label.Text = "Output Directory";
             // 
-            // OutputDirectory_Label
+            // OutputDirectory_TextBox
             // 
-            this.OutputDirectory_Label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.OutputDirectory_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.OutputDirectory_Label.Location = new System.Drawing.Point(15, 27);
-            this.OutputDirectory_Label.Name = "OutputDirectory_Label";
-            this.OutputDirectory_Label.Size = new System.Drawing.Size(186, 20);
-            this.OutputDirectory_Label.TabIndex = 2;
+            this.OutputDirectory_TextBox.Location = new System.Drawing.Point(15, 27);
+            this.OutputDirectory_TextBox.Name = "OutputDirectory_TextBox";
+            this.OutputDirectory_TextBox.Size = new System.Drawing.Size(332, 20);
+            this.OutputDirectory_TextBox.TabIndex = 2;
             // 
             // OutputDirectoryBrowse_Button
             // 
             this.OutputDirectoryBrowse_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.OutputDirectoryBrowse_Button.Location = new System.Drawing.Point(207, 27);
+            this.OutputDirectoryBrowse_Button.Location = new System.Drawing.Point(353, 27);
             this.OutputDirectoryBrowse_Button.Name = "OutputDirectoryBrowse_Button";
             this.OutputDirectoryBrowse_Button.Size = new System.Drawing.Size(75, 20);
             this.OutputDirectoryBrowse_Button.TabIndex = 3;
             this.OutputDirectoryBrowse_Button.Text = "Browse...";
             this.OutputDirectoryBrowse_Button.UseVisualStyleBackColor = true;
+            this.OutputDirectoryBrowse_Button.Click += new System.EventHandler(this.OutputDirectoryBrowse_Button_Click);
             // 
             // SpreadsheetID_Label
             // 
             this.SpreadsheetID_Label.AutoSize = true;
-            this.SpreadsheetID_Label.Location = new System.Drawing.Point(15, 54);
+            this.SpreadsheetID_Label.Location = new System.Drawing.Point(12, 54);
             this.SpreadsheetID_Label.Name = "SpreadsheetID_Label";
             this.SpreadsheetID_Label.Size = new System.Drawing.Size(81, 13);
             this.SpreadsheetID_Label.TabIndex = 4;
@@ -91,14 +94,14 @@
             // 
             this.SpreadsheetID_Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.SpreadsheetID_Textbox.Location = new System.Drawing.Point(18, 71);
+            this.SpreadsheetID_Textbox.Location = new System.Drawing.Point(15, 70);
             this.SpreadsheetID_Textbox.Name = "SpreadsheetID_Textbox";
-            this.SpreadsheetID_Textbox.Size = new System.Drawing.Size(264, 20);
+            this.SpreadsheetID_Textbox.Size = new System.Drawing.Size(410, 20);
             this.SpreadsheetID_Textbox.TabIndex = 5;
             // 
             // SpreadsheetClear_Button
             // 
-            this.SpreadsheetClear_Button.Location = new System.Drawing.Point(18, 98);
+            this.SpreadsheetClear_Button.Location = new System.Drawing.Point(15, 96);
             this.SpreadsheetClear_Button.Name = "SpreadsheetClear_Button";
             this.SpreadsheetClear_Button.Size = new System.Drawing.Size(75, 23);
             this.SpreadsheetClear_Button.TabIndex = 6;
@@ -109,7 +112,7 @@
             // Columns_Button
             // 
             this.Columns_Button.AutoSize = true;
-            this.Columns_Button.Location = new System.Drawing.Point(18, 128);
+            this.Columns_Button.Location = new System.Drawing.Point(12, 129);
             this.Columns_Button.Name = "Columns_Button";
             this.Columns_Button.Size = new System.Drawing.Size(47, 13);
             this.Columns_Button.TabIndex = 7;
@@ -117,7 +120,7 @@
             // 
             // Columns_NumericUpDown
             // 
-            this.Columns_NumericUpDown.Location = new System.Drawing.Point(21, 145);
+            this.Columns_NumericUpDown.Location = new System.Drawing.Point(15, 145);
             this.Columns_NumericUpDown.Minimum = new decimal(new int[] {
             1,
             0,
@@ -141,16 +144,42 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Columns_DataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.Columns_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Columns_DataGridView.Location = new System.Drawing.Point(21, 171);
+            this.Columns_DataGridView.Location = new System.Drawing.Point(15, 171);
             this.Columns_DataGridView.Name = "Columns_DataGridView";
-            this.Columns_DataGridView.Size = new System.Drawing.Size(265, 72);
+            this.Columns_DataGridView.Size = new System.Drawing.Size(411, 72);
             this.Columns_DataGridView.TabIndex = 9;
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save_Button.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Save_Button.Location = new System.Drawing.Point(15, 249);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(410, 62);
+            this.Save_Button.TabIndex = 10;
+            this.Save_Button.Text = "Save";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            // 
+            // IdPaste_Button
+            // 
+            this.IdPaste_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.IdPaste_Button.Location = new System.Drawing.Point(349, 96);
+            this.IdPaste_Button.Name = "IdPaste_Button";
+            this.IdPaste_Button.Size = new System.Drawing.Size(75, 23);
+            this.IdPaste_Button.TabIndex = 11;
+            this.IdPaste_Button.Text = "Paste";
+            this.IdPaste_Button.UseVisualStyleBackColor = true;
+            this.IdPaste_Button.Click += new System.EventHandler(this.IdPaste_Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 489);
+            this.ClientSize = new System.Drawing.Size(444, 323);
+            this.Controls.Add(this.IdPaste_Button);
+            this.Controls.Add(this.Save_Button);
             this.Controls.Add(this.Columns_DataGridView);
             this.Controls.Add(this.Columns_NumericUpDown);
             this.Controls.Add(this.Columns_Button);
@@ -158,10 +187,11 @@
             this.Controls.Add(this.SpreadsheetID_Textbox);
             this.Controls.Add(this.SpreadsheetID_Label);
             this.Controls.Add(this.OutputDirectoryBrowse_Button);
-            this.Controls.Add(this.OutputDirectory_Label);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OutputDirectory_TextBox);
+            this.Controls.Add(this.Outputdirectory_Label);
             this.Controls.Add(this.label1);
-            this.MinimumSize = new System.Drawing.Size(314, 528);
+            this.MaximumSize = new System.Drawing.Size(5000, 362);
+            this.MinimumSize = new System.Drawing.Size(460, 362);
             this.Name = "Form1";
             this.Text = "Sheets2CSV Settings";
             ((System.ComponentModel.ISupportInitialize)(this.Columns_NumericUpDown)).EndInit();
@@ -174,9 +204,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.TextBox OutputDirectory_Label;
+        private System.Windows.Forms.Label Outputdirectory_Label;
+        private System.Windows.Forms.TextBox OutputDirectory_TextBox;
         private System.Windows.Forms.Button OutputDirectoryBrowse_Button;
         private System.Windows.Forms.Label SpreadsheetID_Label;
         private System.Windows.Forms.TextBox SpreadsheetID_Textbox;
@@ -184,6 +213,9 @@
         private System.Windows.Forms.Label Columns_Button;
         private System.Windows.Forms.NumericUpDown Columns_NumericUpDown;
         private System.Windows.Forms.DataGridView Columns_DataGridView;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button IdPaste_Button;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- sheetstocsv/SettingsUI/Form1.cs
+++ sheetstocsv/SettingsUI/Form1.cs
@@ -29,7 +29,20 @@ namespace SettingsUI
 
         private void button2_Click(object sender, EventArgs e)
         {
+            SpreadsheetID_Textbox.Clear();
+        }
 
+        private void IdPaste_Button_Click(object sender, EventArgs e)
+        {
+            SpreadsheetID_Textbox.Text = Clipboard.GetText();
+        }
+
+        private void OutputDirectoryBrowse_Button_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                OutputDirectory_TextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
         }
     }
 }
